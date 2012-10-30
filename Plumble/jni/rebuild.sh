82dd968c498d433449f382eb97a28afa2aef0ee2
Ankush Gupta <amgupt01@gmail.com>
2012-10-30 05:42:40 -0400

    Whoops

--- Plumble/jni/rebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-javac -cp ../libs/hawtjni-runtime-1.1-SNAPSHOT.jar ../src/com/morlunk/mumbleclient/jni/Native.java
-java -cp hawtjni-generator-1.1-SNAPSHOT.jar org.fusesource.hawtjni.generator.HawtJNI -o new ../src
